ee7a2e5cc86c77b820ae83c7c6ce2db855b03e79
Nikhil <nikhilprajapati1411@gmail.com>
2024-07-27 22:21:26 +0530

    implemented sentry and footer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <main>
+      <main className="flex font-inter justify-between w-full min-h-screen">
           {children} 
+          <div className="auth-asset">
+            <div>
+              <Image src="/icons/auth-image.svg" alt="auth-image" width={500} height={500}/>
+            </div>
+          </div>
       </main>
     );
   }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,16 @@
+import { logoutAccount } from '@/lib/actions/user.actions'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const Footer = ({user,type='desktop'}: FooterProps) => {
+
+    const router=useRouter();
+    const handleLogOut=async()=>{
+        const loggedOut=await logoutAccount();
+
+        if(loggedOut) router.push('/sign-in');
+    }
   return (
    <footer className='footer'>
     <div className={type==='mobile'?'footer_name-mobile':'footer_name'}>
@@ -15,6 +25,9 @@ const Footer = ({user,type='desktop'}: FooterProps) => {
         <p className="text-14 truncate font-normal text-gray-600">
             {user.email}
         </p>
+    </div>
+    <div className="footer_image" onClick={handleLogOut}>
+       <Image src="icons/logout.svg" fill alt='jsm'/>
     </div>
    </footer>
   )
